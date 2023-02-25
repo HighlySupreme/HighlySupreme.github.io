221bbe42baf49ac79db2d2900aa56ce2e931bbc9
HighlySupreme <gal.1999k@gmail.com>
2023-02-25 11:31:06 +0100

    updates

---
+++ Custom/JS/main.js
@@ -0,0 +1,26 @@
+/*scroll on click effect*/
+$(function(){
+    $(".on-click").on('click', function(){
+        $("html, body").animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
+
+/*mobile navbar close on click*/
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+AOS.init({
+disable: window.innerWidth < 768,
+easing: 'ease-out-back',
+duration: 1000
+});
+
+AOS.init({
+    disable: function() {
+        var maxWidth = 800;
+        return window.innerWidth < maxWidth;
+    }
+});
